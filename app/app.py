1bc1dc9070e145207760728681690ed5151ba891
DaviPierre <zafirosafira@gmail.com>
2026-03-24 15:54:36 -0300

    feat: Add users authentification

--- app/app.py
+++ app/app.py
@@ -9,6 +9,8 @@ import shutil
 import os
 import uuid
 import tempfile
+from app.users import auth_backend, fastapi_users, current_active_user
+from app.schemas import UserRead, UserCreate, UserUpdate
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +18,12 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
 
 @app.post("/upload/")
 async def upload_file(
